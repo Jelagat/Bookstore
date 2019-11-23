f40c6c1b6388d96ff2e23199442fdc573d14b0a0
Jelagat <irnjelagat@gmail.com>
2019-11-22 23:01:04 -0800

    first commit

---
+++ Bookstore/Model/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Model
+{
+    public class Book
+    {
+        [key]
+        public int Id { get; set; }
+        [Reguired]
+        public string Name { get; set; }
+        public string Author { get; set }
+    }
+}
